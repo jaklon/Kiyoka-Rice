a0ae070dbf4520d7ae3a4fb159c2a2f045b02002
Jascon <jasconkembuan9@gmail.com>
2025-06-10 13:14:40 +0700

    pullin

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ function typeEffect(element, speed = 100) {
 document.querySelectorAll(".animated-text").forEach((el) => {
   typeEffect(el, 50); // Kurangi speed untuk lebih cepat
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("[data-bs-toggle='modal']").forEach((button) => {
+    button.addEventListener("click", function (event) {
+      let modal = document.querySelector("data-bs-target"); // Ganti dengan ID modal kamu
+      let rect = this.getBoundingClientRect(); // Ambil posisi tombol yang ditekan
+
+      modal.style.position = "absolute";
+      modal.style.top = `${rect.top + window.scrollY}px`;
+      modal.style.left = `${rect.left + window.scrollX}px`;
+    });
+  });
+});
+
+document
+  .getElementById("moreInfoBtn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+  });
